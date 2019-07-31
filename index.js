7541816b296373bb0a0efe6754a1e1671fafe4bd
Ben Edwards <hello@benedfit.com>
2019-07-31 10:24:19 +0100

    Initial release

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: ['stylelint-prettier/recommended', 'stylelint-config-standard'],
+    plugins: ['stylelint-scss'],
+    rules: {
+        // Adds support for SCSS @ rules
+        'at-rule-no-unknown': null,
+        'scss/at-rule-no-unknown': true,
+
+        // Disables descending specificity rules
+        'no-descending-specificity': null
+    },
+    syntax: 'scss'
+}
